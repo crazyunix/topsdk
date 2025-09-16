da47d529dd6103dde2607afa9dfea3cc091a9a83
木子~ <muzi@muzi.com>
2025-09-16 18:07:54 +0800

    新增 132 接口

---
+++ ability132/response/TaobaoTmcGroupsGetResponse.go
@@ -0,0 +1,25 @@
+package response
+
+import "github.com/crazyunix/topsdk/ability132/domain"
+
+type TaobaoTmcGroupsGetResponse struct {
+
+	/*
+	   System request id
+	*/
+	RequestId string `json:"request_id,omitempty" `
+
+	/*
+	   System body
+	*/
+	Body string
+
+	/*
+	   dasdasd
+	*/
+	Groups []domain.TaobaoTmcGroupsGetTmcGroup `json:"groups,omitempty" `
+	/*
+	   分组总数
+	*/
+	TotalResults int64 `json:"total_results,omitempty" `
+}
